b8e0b8f131af9bca6ddd947f0fb4b11b2cf5b2c0
Noé <user@sceeker.com>
2023-04-18 16:53:46 +0200

    J'ai dis robot chat

---
+++ src/ins.java
@@ -0,0 +1,16 @@
+import fr.emse.fayol.maqit.simulator.robot.GridTurtlebot;
+
+public class ins extends GridTurtlebot {
+
+    public ins(int id, String name, int field, int debug, int[] pos, int r, int c) {
+        super(id, name, field, debug, pos, r, c);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void move(int arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+    
+}
