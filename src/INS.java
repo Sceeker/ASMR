78cdfd5f23460c0edf82edeeb7681ea619be6d4b
sceeker <user@sceeker.com>
2023-05-08 15:40:42 +0200

    .

--- src/INS.java
+++ src/INS.java
@@ -1,10 +1,26 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import fr.emse.fayol.maqit.simulator.robot.GridTurtlebot;
+import fr.emse.fayol.maqit.simulator.environment.Cell;
+import fr.emse.fayol.maqit.simulator.environment.GridManagement;
 
 public class INS extends GridTurtlebot {
+    private GridManagement restaurantLayout;
 
-    public INS(int id, String name, int field, int debug, int[] pos, int r, int c) {
+    public INS(int id, String name, int field, int debug, int[] pos, int r, int c, GridManagement env) {
         super(id, name, field, debug, pos, r, c);
-        //TODO Auto-generated constructor stub
+
+        restaurantLayout = env;
+    }
+
+    private GridPath findPath(int x, int y) {
+        ArrayList<SituatedCell> open = new ArrayList<SituatedCell>();
+        LinkedList<SituatedCell> close = new LinkedList<SituatedCell>();
+        GridPath res = new GridPath();
+
+        // COMMENT AVOIR LA PUTAIN DE CELL ??? DU GRID MANAGZEMNT ??? ENV PROTEC ???
+        open.add(new SituatedCell(restaurantLayou, new int[2] = {this.x, this.y}));
     }
 
     public void radioReception(RadioData dat) {
