815afe851a6b0c65167b5e9d930ad15e7bc7342c
PA <pierrearnaud.godel@hotmail.fr>
2023-05-08 15:42:34 +0200

    followPath

--- src/INS.java
+++ src/INS.java
@@ -1,3 +1,4 @@
+import fr.emse.fayol.maqit.simulator.components.Orientation;
 import fr.emse.fayol.maqit.simulator.robot.GridTurtlebot;
 
 public class INS extends GridTurtlebot {
@@ -20,6 +21,63 @@ public class INS extends GridTurtlebot {
     public void move(int arg0) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+
+    public void followPath (int[][] coord) {
+        for (int i = 0; i < coord.length; i++) {
+            if (coord[i + 1][0] - coord[i][0] > 0) {
+                if (getCurrentOrientation() == Orientation.up) {
+                    this.moveRight();
+                }
+                if (getCurrentOrientation() == Orientation.down) {
+                    this.moveLeft();
+                }
+                if (getCurrentOrientation() == Orientation.left) {
+                    this.moveRight();
+                    this.moveRight();
+                }
+            }
+
+            if (coord[i + 1][0] - coord[i][0] < 0) {
+                if (getCurrentOrientation() == Orientation.up) {
+                    this.moveLeft();
+                }
+                if (getCurrentOrientation() == Orientation.down) {
+                    this.moveRight();
+                }
+                if (getCurrentOrientation() == Orientation.right) {
+                    this.moveRight();
+                    this.moveRight();
+                }
+            }
+
+            if (coord[i + 1][1] - coord[i][1] > 0) {
+                if (getCurrentOrientation() == Orientation.left) {
+                    this.moveRight();
+                }
+                if (getCurrentOrientation() == Orientation.right) {
+                    this.moveLeft();
+                }
+                if (getCurrentOrientation() == Orientation.down) {
+                    this.moveRight();
+                    this.moveRight();
+                }
+            }
+
+                if (coord[i + 1][1] - coord[i][1] < 0) {
+                    if (getCurrentOrientation() == Orientation.left) {
+                        this.moveLeft();
+                    }
+                    if (getCurrentOrientation() == Orientation.right) {
+                        this.moveRight();
+                    }
+                    if (getCurrentOrientation() == Orientation.up) {
+                        this.moveRight();
+                        this.moveRight();
+                    }
+            }
+            this.moveForward();
+        }
     }
 
 
